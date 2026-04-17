961b1bd4cb0c34c13eebdb40bd710ab8f8c79c3d
Lilly <149632943+lillyjacks@users.noreply.github.com>
2026-04-17 14:14:30 -0400

    working on assign 5

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,6 @@
 import { getFirestore } from "firebase/firestore";
 import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
   //   // COPY this from your Firebase Console
@@ -9,9 +10,22 @@ const firebaseConfig = {
   //   projectId: "your-project-name-here",
   //   storageBucket: "your-project-name.appspot.com",
   //   messagingSenderId: "xxxxxxxx",
+
+  apiKey: "AIzaSyCYn8aEWYLPjpKscryLnBKwEoOFGqZQjWk",
+
+  authDomain: "assignment5-b94b8.firebaseapp.com",
+
+  projectId: "assignment5-b94b8",
+
+  storageBucket: "assignment5-b94b8.firebasestorage.app",
+
+  messagingSenderId: "354028679895",
+
+  // appId: "1:354028679895:web:bf7efe509f41d81e2b3692"
+
 };
 
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-export default db;
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
